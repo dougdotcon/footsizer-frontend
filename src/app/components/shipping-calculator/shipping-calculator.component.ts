49e08c77d82c11fa5c8014375e631453d798bc3d
Joice Paz <joicepazsilva@gmail.com>
2024-10-13 15:55:31 -0300

    Initial components

---
+++ src/app/components/shipping-calculator/shipping-calculator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shipping-calculator',
+  templateUrl: './shipping-calculator.component.html',
+  styleUrls: ['./shipping-calculator.component.css']
+})
+export class ShippingCalculatorComponent {
+
+}
